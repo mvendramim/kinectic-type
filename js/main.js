d0123963b6dc42af0a327885a9a82272b7ba687f
Mayra Vendramim <may.vendramim@gmail.com>
2014-04-02 07:25:25 -0400

    last

--- js/main.js
+++ js/main.js
@@ -50,6 +50,21 @@ $(function() {
 });
 
 $(function() {
+  $('a[href*=#animation]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top -200
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+$(function() {
   $('a[href*=#thepoem]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
